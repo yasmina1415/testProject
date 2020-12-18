d685fc07952bc471422b9facc5393a50c02eacf0
yasmina <yasminaabdelazim@gmail.com>
2020-12-18 14:18:35 +0200

    first commit (main class)

---
+++ main.java
@@ -0,0 +1,21 @@
+public class main {
+	
+	public void printHello(){System.out.println("Hello !");}
+	
+	
+	public static void main (String args []){
+		
+		main m = new main();
+		m.printHello();
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+}
